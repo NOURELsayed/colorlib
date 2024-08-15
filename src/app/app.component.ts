29e954f3316da4078101df3c1731b14b951f4596
SHmohamed229 <SHmohamed229@gmail.com>
2024-08-15 22:57:05 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/components/header/header.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-new-version-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // {path:'header', component: HeaderComponent},
+
+];
