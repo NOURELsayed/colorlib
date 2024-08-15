666f5f2ecfaf6fd27cab4ee838c734a4ba67914c
unknown <ne483925@gmail.com>
2024-08-16 01:56:12 +0300

    main categories card added, update ngx version and add min file for primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
+import { CategoryCardComponent } from './pages/home-page/category-card/category-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CategoryCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
